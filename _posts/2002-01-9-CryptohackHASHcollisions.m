b6ff0d0049768d1111f4af8615109841abebf0ef
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-01-14 10:03:03 +1000

    Create 2002-01-9-CryptohackHASHcollisions.m

---
+++ _posts/2002-01-9-CryptohackHASHcollisions.m
@@ -0,0 +1,11 @@
+---
+title: HASH FUNCTION - Collisions
+categories:
+- CryptoHack
+excerpt: |
+  
+---
+
+This is my writeup of the [CryptoHack Hash Functions challenges](https://cryptohack.org/challenges/hashes/).
+
+### Collider
